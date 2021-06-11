6954b4e875a5d75a2e5562bd6ae9f9b080ea4e6b
Giovanna Cunha <giovannakelli@gmail.com>
2021-06-11 17:59:11 -0300

    set up migrations configurations

---
+++ src/migrations/20210604012612-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      passwordResetToken: {
+        type: Sequelize.STRING,
+      },
+      passwordResetExpires: {
+        type: Sequelize.DATE,
+      },
+      emailConfirmationToken: {
+        type: Sequelize.STRING,
+      },
+      emailConfirmationExpires: {
+        type: Sequelize.DATE,
+      },
+      isVerified: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
