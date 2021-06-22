c16da2e922a923e5c15855acdf689b070dc8456c
Giovanna Cunha <giovannakelli@gmail.com>
2021-06-21 23:30:17 -0300

    remove spaces

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,7 +1,7 @@
-const {fileLoader, mergeTypes} = require('merge-graphql-schemas');
+const { fileLoader, mergeTypes } = require('merge-graphql-schemas');
 const path = require('path');
 
 const typesArray = fileLoader(path.join(__dirname, 'modules', '**', '*.gql'));
-const typeDefs = mergeTypes(typesArray)
+const typeDefs = mergeTypes(typesArray);
 
 module.exports = typeDefs;
