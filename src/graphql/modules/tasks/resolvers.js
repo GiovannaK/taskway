0570926cd69f838068da4492d6ca5d8b5c719e4f
Giovanna Cunha <giovannakelli@gmail.com>
2021-06-22 00:06:25 -0300

    show tasks by workspace functionality was created
    Only workspace owner and workspace invites users can see tasks

--- src/graphql/modules/tasks/resolvers.js
+++ src/graphql/modules/tasks/resolvers.js
@@ -1,8 +1,50 @@
+/* eslint-disable camelcase */
 const { ApolloError, AuthenticationError } = require('apollo-server-errors');
 const auth = require('../../../middlewares/auth');
-const { Task, Workspace } = require('../../../models');
+const { Task, Workspace, User_Workspaces } = require('../../../models');
 
 module.exports = {
+  Query: {
+    tasks: async (_, { workspaceId }, context) => {
+      try {
+        auth(context);
+        const { userId } = context.req;
+
+        const workspace = await Workspace.findOne({
+          where: {
+            id: workspaceId,
+          },
+        });
+
+        if (!workspace) {
+          throw new ApolloError('Workspace not exist');
+        }
+
+        const canViewTask = await User_Workspaces.findOne({
+          where: {
+            workspaceId,
+            userId,
+          },
+        });
+
+        if (!canViewTask && userId !== workspace.ownerId) {
+          throw new AuthenticationError(
+            'You are not authorized to see tasks in this workspace',
+          );
+        }
+
+        const tasks = await Task.findAll({
+          where: {
+            workspaceId,
+          },
+        });
+
+        return tasks;
+      } catch (error) {
+        throw new ApolloError('Cannot show tasks for this workspace');
+      }
+    },
+  },
   Mutation: {
     createTask: async (_, {
       title,
