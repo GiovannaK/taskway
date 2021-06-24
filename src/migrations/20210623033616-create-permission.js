31d58a9b76cee937f12f839f647780adb60ded99
Giovanna Cunha <giovannakelli@gmail.com>
2021-06-23 21:46:14 -0300

    Permission tasble was created and seeds were added to populate permissions

---
+++ src/migrations/20210623033616-create-permission.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Permissions', {
+      id: {
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Permissions');
+  },
+};
---
+++ src/models/permission.js
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Permission extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      /* this.belongsToMany(User_Workspaces, { foreignKey: 'permissionId', through: 'User_Workspaces', as: 'permissions_users' }); */
+    }
+  }
+  Permission.init({
+    id: {
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      type: DataTypes.UUID,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'Permission',
+  });
+  return Permission;
+};
