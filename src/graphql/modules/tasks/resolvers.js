57256d26deaa5829b4ae923e8248b4c3f657878d
Giovanna Cunha <giovannakelli@gmail.com>
2021-06-24 21:43:03 -0300

    permission was added to update task resolver only workspace member that have the correct permission or workspace owner can update tasks

--- src/graphql/modules/tasks/resolvers.js
+++ src/graphql/modules/tasks/resolvers.js
@@ -79,5 +79,57 @@ module.exports = {
         throw new ApolloError('Cannot create task');
       }
     },
+    updateTask: async (_, {
+      id,
+      title,
+      workspaceId,
+      link,
+      maxDate,
+      description,
+    }, context) => {
+      try {
+        auth(context);
+        const { userId } = context.req;
+
+        const workspace = await Workspace.findByPk(workspaceId);
+
+        if (!workspace) {
+          throw new ApolloError('Cannot found workspace');
+        }
+
+        const getPermission = await Permission.findOne({
+          where: {
+            name: 'updateTask',
+          },
+        });
+
+        if (!getPermission) {
+          throw new ApolloError('Cannot find permission');
+        }
+
+        const owner = await isWorkspaceOwner(workspaceId, userId);
+        const userWorkspaceHasPerm = await checkPermission(userId, workspaceId, getPermission.id);
+
+        if (!owner && !userWorkspaceHasPerm) {
+          throw new ForbiddenError('Unauthorized to update task in this workspace');
+        }
+
+        const task = await Task.findByPk(id);
+
+        if (!task) throw new ApolloError('Cannot update task, Task not exist');
+
+        task.title = title;
+        task.link = link;
+        task.maxDate = maxDate;
+        task.description = description;
+
+        await task.save();
+
+        return task;
+      } catch (error) {
+        console.log(error);
+        throw new ApolloError('Cannot create task');
+      }
+    },
   },
 };
