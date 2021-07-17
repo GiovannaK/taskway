46b31af655e1104278a05cd9cf4517c55374322c
Giovanna Cunha <giovannakelli@gmail.com>
2021-07-16 21:25:20 -0300

    new Error was replaced by ApolloError class to show correct error message

--- src/graphql/modules/workspaces/resolvers.js
+++ src/graphql/modules/workspaces/resolvers.js
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-const { UserInputError, ForbiddenError } = require('apollo-server-errors');
+const { UserInputError, ForbiddenError, ApolloError } = require('apollo-server-errors');
 const auth = require('../../../middlewares/auth');
 const isWorkspaceMember = require('../../../middlewares/isWorkspaceMember');
 const isWorkspaceOwner = require('../../../middlewares/isWorkspaceOwner');
@@ -34,7 +34,7 @@ module.exports = {
         });
         return workspaces;
       } catch (error) {
-        throw new Error('Cannot found Workspaces for this user', { error });
+        throw new ApolloError('Cannot found Workspaces for this user', { error });
       }
     },
     workspaceMember: async (_, __, context) => {
@@ -54,7 +54,7 @@ module.exports = {
 
         return workspaces;
       } catch (error) {
-        throw new Error('Cannot found Workspaces for this user', { error });
+        throw new ApolloError('Cannot found Workspaces for this user', { error });
       }
     },
     workspace: async (_, { id }, context) => {
@@ -71,7 +71,7 @@ module.exports = {
 
         return workspace;
       } catch (error) {
-        throw new Error('Cannot find workspace', { error });
+        throw new ApolloError('Cannot find workspace', { error });
       }
     },
     usersWorkspace: async (_, { id }, context) => {
@@ -94,7 +94,7 @@ module.exports = {
         return workspace.users;
       } catch (error) {
         console.log(error);
-        throw new Error('Cannot show users for this workspace', { error });
+        throw new ApolloError('Cannot show users for this workspace', { error });
       }
     },
   },
@@ -112,7 +112,7 @@ module.exports = {
         return workspace;
       } catch (error) {
         console.log(error);
-        throw new Error('Workspace could not be created', { error });
+        throw new ApolloError('Workspace could not be created', { error });
       }
     },
     addUserToWorkspace: async (_, args, context) => {
@@ -154,7 +154,7 @@ module.exports = {
         return addedUser;
       } catch (error) {
         console.log(error);
-        throw new Error('Cannot add User to workspace', { error });
+        throw new ApolloError('Cannot add User to workspace', { error });
       }
     },
     removeUserFromWorkspace: async (_, args, context) => {
@@ -192,7 +192,7 @@ module.exports = {
         return !!removedUser;
       } catch (error) {
         console.log(error);
-        throw new Error('Cannot delete user from workspace', { error });
+        throw new ApolloError('Cannot delete user from workspace', { error });
       }
     },
   },
