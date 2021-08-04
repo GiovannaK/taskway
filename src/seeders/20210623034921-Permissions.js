767dda9e369603259045acf142f2c82ee0c717c9
Giovanna Cunha <giovannakelli@gmail.com>
2021-08-04 20:00:31 -0300

    fix: npm start now run sequelize migrations

--- src/seeders/20210623034921-Permissions.js
+++ src/seeders/20210623034921-Permissions.js
@@ -1,21 +1,23 @@
 /* eslint-disable no-unused-vars */
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Permissions', [
       {
-        id: 'b6124847-1890-4148-b2d1-509eb70fb44f',
+        id: uuidv4(),
         name: 'createTask',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        id: 'a58c53a3-7720-4c11-8da0-815abcf57f13',
+        id: uuidv4(),
         name: 'updateTask',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        id: 'ded7c01d-f224-4480-8ea3-50403de368a0',
+        id: uuidv4(),
         name: 'deleteTask',
         createdAt: new Date(),
         updatedAt: new Date(),
