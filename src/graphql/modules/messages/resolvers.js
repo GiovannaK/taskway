5e41c6b003dd89883eb6c302754689c5538cda9d
Giovanna Cunha <giovannakelli@gmail.com>
2021-08-08 22:37:44 -0300

    query to find all messages per room was added, resolver to create messages was added, only owners and members can perform this action

---
+++ src/graphql/modules/messages/resolvers.js
@@ -0,0 +1,72 @@
+const { ForbiddenError, ApolloError, UserInputError } = require('apollo-server-errors');
+const auth = require('../../../middlewares/auth');
+const isWorkspaceMember = require('../../../middlewares/isWorkspaceMember');
+const isWorkspaceOwner = require('../../../middlewares/isWorkspaceOwner');
+const { Message } = require('../../../models');
+
+module.exports = {
+  Query: {
+    messagesPerRoom: async (_, { roomId, workspaceId }, context) => {
+      try {
+        auth(context);
+        const { userId } = context.req;
+        const owner = await isWorkspaceOwner(workspaceId, userId);
+        const workspaceMember = await isWorkspaceMember(workspaceId, userId);
+
+        if (!owner && !workspaceMember) {
+          throw new ForbiddenError('Not authorized to send messages in this room');
+        }
+
+        const messages = await Message.findAll({
+          where: {
+            roomId,
+          },
+          include: {
+            association: 'messages_user',
+            include: {
+              association: 'profile',
+            },
+          },
+        });
+
+        if (!messages) {
+          throw new ApolloError('Cannot show messages');
+        }
+
+        return messages;
+      } catch (error) {
+        throw new ApolloError('Cannot show message in this workspace', { error });
+      }
+    },
+  },
+  Mutation: {
+    createMessage: async (_, {
+      roomId, body, workspaceId,
+    }, context) => {
+      try {
+        auth(context);
+        const { userId } = context.req;
+        const owner = await isWorkspaceOwner(workspaceId, userId);
+        const workspaceMember = await isWorkspaceMember(workspaceId, userId);
+
+        if (!owner && !workspaceMember) {
+          throw new ForbiddenError('Not authorized to send messages in this room');
+        }
+
+        const message = await Message.create({
+          roomId,
+          userId,
+          body,
+        });
+
+        if (!message) {
+          throw new UserInputError('Cannot add message, verify please');
+        }
+
+        return message;
+      } catch (error) {
+        throw new ApolloError('Cannot create message in this workspace', { error });
+      }
+    },
+  },
+};
