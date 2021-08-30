497bb4d8383cf56e718867867e194e8852bc356d
Giovanna Cunha <giovannakelli@gmail.com>
2021-08-30 19:41:05 -0300

    feat: subscriptions now are configured to use redis on development and production mode

--- src/modules/pubSub.js
+++ src/modules/pubSub.js
@@ -1,4 +1,12 @@
 /* eslint-disable import/prefer-default-export */
-const { PubSub } = require('apollo-server-express');
+/* const { PubSub } = require('apollo-server-express'); */
 
-exports.pubsub = new PubSub();
+const { RedisPubSub } = require('graphql-redis-subscriptions');
+
+exports.pubsub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    retryStrategy: (options) => Math.max(options.attempt * 100, 3000),
+  },
+});
